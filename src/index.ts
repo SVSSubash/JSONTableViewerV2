57c3b060467a0963998374b612e004e09b68e615
SVSSubash <44859012+SVSSubash@users.noreply.github.com>
2023-06-22 12:00:28 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,32 +8,35 @@ export class JSONManager
 {
     public static getHTML(jsonData:string):string
     {
-        // 1. Get Table
-        let viewer = new JsonViewer(false);
-        let tableString = viewer.getTable(jsonData,0);
-
-        // 2. Get Common Styling
-        let cssCommon = new CSSCommon();
-        let cssCommonStyling = cssCommon.getCSSCommonStyling();
-
-        // 2. Get Style
+        // 1. Get configuration
         let jsonTableViewerConfiguration = vscode.workspace.getConfiguration("jsonTableViewer");
         let jsonTableViewerTheme:string = "";
         let jsonTableViewerDensity:string = "";
         let jsonTableViewerFontStyle:string = "";
+        let jsonShowBraceNumbers:boolean = true;
         if (jsonTableViewerConfiguration) 
         {
             jsonTableViewerTheme = jsonTableViewerConfiguration.get("jsonTableViewerTheme");
             jsonTableViewerDensity = jsonTableViewerConfiguration.get("jsonTableViewerDensity");
             jsonTableViewerFontStyle = jsonTableViewerConfiguration.get("jsonTableViewerFontStyle");
+            jsonShowBraceNumbers = jsonTableViewerConfiguration.get("jsonShowBraceNumbers");
         }
 
+        // 2. Get Table
+        let viewer = new JsonViewer(false);
+        let tableString = viewer.getTable(jsonData,0,jsonShowBraceNumbers);
+
+        // 3. Get Common Styling
+        let cssCommon = new CSSCommon();
+        let cssCommonStyling = cssCommon.getCSSCommonStyling();
+
+        // 4. Get theme styling
         let cssOptionsSetter = new CSSOptionsSetter(jsonTableViewerTheme,jsonTableViewerDensity,jsonTableViewerFontStyle);
 
-        // 2. Get Global Styling
+        // 5. Get Global Styling
         let globalCSSStyling = cssCommonStyling + cssOptionsSetter.getTheme() + cssOptionsSetter.getDensity() + cssOptionsSetter.getFontStyle();
 
-        // 3. Render HTML
+        // 6. Render HTML
         let htmlRenderer = new HTMLRenderer(jsonTableViewerDensity,tableString,globalCSSStyling);
         return htmlRenderer.renderHTML();
     }
